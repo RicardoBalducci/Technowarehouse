5243e15c57c81c87efcd8d26f901efaf84da481b
Ricardo Balducci <Bandrock2806@gmail.com>
2024-07-14 12:10:41 -0400

    Update

--- technowarehouse/src/module/admin/pages/principal/principal.admin.tsx
+++ technowarehouse/src/module/admin/pages/principal/principal.admin.tsx
@@ -2,9 +2,32 @@ import React, { useState, useEffect } from "react";
 import Menu from "../components/menu";
 import { supabase } from "../../../../services/supabase";
 import styles from "./principal.admin.module.css";
-//HAcer que se muestre la imagen
+interface ImageObject {
+  name: string;
+  // Otras propiedades de la imagen, si las hay
+}
 function PrincipalAdmin() {
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [imageUrl, setImageUrl] = useState(""); // Estado para almacenar la URL de la imagen
+  const [imageList, setImageList] = useState<ImageObject[]>([]); // Estado para almacenar la lista de imágenes
+
+  useEffect(() => {
+    verTodasLasImagenes();
+  }, []);
+
+  async function verTodasLasImagenes() {
+    const { data, error } = await supabase.storage
+      .from("Technowarehouse")
+      .list("public/");
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    if (data) {
+      setImageList(data);
+    }
+  }
 
   const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -15,8 +38,10 @@ function PrincipalAdmin() {
         .upload("public/" + file.name, file);
 
       if (data) {
-        const publicUrl = data.path; // Verifica la propiedad correcta en el objeto de respuesta
-        setImageUrl(publicUrl);
+        alert("Subido");
+        const url = `https://dkmcywdlzsslpgnvfxzy.supabase.co/storage/v1/object/public/Technowarehouse/public/${file.name}`; // Obtener la URL de la imagen subida
+        setImageUrl(url); // Actualizar el estado con la URL de la imagen subida
+        verTodasLasImagenes(); // Actualizar la lista de imágenes
       } else if (error) {
         console.log(error);
       }
@@ -31,9 +56,13 @@ function PrincipalAdmin() {
       <div className={styles.content}>
         <h1>Panel de Control</h1>
         <input type="file" accept="image/*" onChange={handleUpload} />
-        {imageUrl && (
-          <img src={imageUrl} alt="Imagen subida" className={styles.img} />
-        )}
+        {imageUrl && <img src={imageUrl} alt="Imagen subida" />}
+        <h2>Imágenes subidas:</h2>
+        <ul>
+          {imageList.map((image) => (
+            <img src={image.name} alt="" />
+          ))}
+        </ul>
       </div>
     </div>
   );
@@ -41,30 +70,37 @@ function PrincipalAdmin() {
 
 export default PrincipalAdmin;
 /*
-//import { Link } from "react-router-dom";
-import styles from "./principal.admin.module.css";
-import React, { ChangeEvent } from "react";
+import React, { useState, useEffect } from "react";
 import Menu from "../components/menu";
 import { supabase } from "../../../../services/supabase";
-
+import styles from "./principal.admin.module.css";
+import { verTodasLasImagenes } from "../../../../services/supabase";
 function PrincipalAdmin() {
-  const handleUpload = async (e: ChangeEvent<HTMLInputElement>) => {
-    let file;
+  const [imageUrl, setImageUrl] = useState(""); // Estado para almacenar la URL de la imagen
 
-    if (e.target.files) {
-      file = e.target.files[0];
-    }
+  useEffect(() => {
+    verTodasLasImagenes();
+  }, []);
 
-    const { data, error } = await supabase.storage
-      .from("Technowarehouse")
-      .upload("public" + file?.name, file as File);
+  const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
 
-    if (data) {
-      console.log(data);
-    } else if (error) {
-      console.log(error);
+    if (file) {
+      const { data, error } = await supabase.storage
+        .from("Technowarehouse")
+        .upload("public/" + file.name, file);
+
+      if (data) {
+        alert("Subido");
+        const url = `https://dkmcywdlzsslpgnvfxzy.supabase.co/storage/v1/object/public/Technowarehouse/public/${file.name}`; // Obtener la URL de la imagen subida
+        setImageUrl(url); // Actualizar el estado con la URL de la imagen subida
+        verTodasLasImagenes(); // Actualizar la lista de imágenes
+      } else if (error) {
+        console.log(error);
+      }
     }
   };
+
   return (
     <div className={styles.container}>
       <nav className={styles.menu}>
@@ -73,6 +109,7 @@ function PrincipalAdmin() {
       <div className={styles.content}>
         <h1>Panel de Control</h1>
         <input type="file" accept="image/*" onChange={handleUpload} />
+        {imageUrl && <img src={imageUrl} alt="Imagen subida" />}{" "}
       </div>
     </div>
   );
