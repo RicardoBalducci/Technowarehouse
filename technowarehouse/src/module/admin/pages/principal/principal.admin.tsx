24214d2418dec46fb80740df483655801113a6db
Ricardo Balducci <Bandrock2806@gmail.com>
2024-07-24 11:09:21 -0400

    Update principal.admin.tsx

--- technowarehouse/src/module/admin/pages/principal/principal.admin.tsx
+++ technowarehouse/src/module/admin/pages/principal/principal.admin.tsx
@@ -4,6 +4,12 @@ import useStockData from "./hook/useStockData";
 import useUserData from "./hook/useUserData";
 import styles from "./principal.admin.module.css";
 import useProveedorData from "./hook/useProveedorData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUsers,
+  faBoxesStacked,
+  faTruckField,
+} from "@fortawesome/free-solid-svg-icons";
 function PrincipalAdmin() {
   const totalStock = useStockData();
   const totalProveedor = useProveedorData();
@@ -16,8 +22,11 @@ function PrincipalAdmin() {
       </nav>
       <div className={styles.content}>
         <h1>Panel de Control</h1>
+        <FontAwesomeIcon icon={faBoxesStacked} />
         <p>Total Stock: {totalStock}</p>
+        <FontAwesomeIcon icon={faUsers} />
         <p>Total Users: {totalUsers}</p>
+        <FontAwesomeIcon icon={faTruckField} />
         <p>Total Proveedor: {totalProveedor}</p>
       </div>
     </div>
