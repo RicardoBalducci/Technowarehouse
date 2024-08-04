245f7c68a338947995ba72a381ebd7ac68c97b59
Aimelq <quijadaaimel@gmail.com>
2024-08-03 23:16:44 -0400

    Carrito de compra

--- technowarehouse/src/App.tsx
+++ technowarehouse/src/App.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Carrito from "./carrito-compras";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 function App(){
   return(
     <BrowserRouter>
     <Routes>
+    
+   
+ 
       <Route path="/" element={<Carrito />}></Route>
     </Routes>
     </BrowserRouter>
--- technowarehouse/src/carrito-compras.tsx
+++ technowarehouse/src/carrito-compras.tsx
@@ -8,52 +8,57 @@ import "../public/ico4.png"
 import "../public/ico5.png"
 import "../public/ico6.png"
 import "../public/icono.png"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import ShoppingCart from './car';
 
 function Carrito(){
     return(
-        <><nav>
+        <><><nav>
             <img className="logo-carrito" src="logo.png" alt="logo"></img>
             <h1 className="titulo-pag">TECHNO WAREHOUSE</h1>
             <div className="buscador">
                 <div className="borde">
-                <input className="buscar" id="buscar-producto" type="text" placeholder="Busca aquí tu producto" />
+                    <input className="buscar" id="buscar-producto" type="text" placeholder="Busca aquí tu producto" />
                 </div>
-                <button className="boton-busqueda" type="submit"> 
+                <button className="boton-busqueda" type="submit">
                     <img className="lupa" src="icono.png" alt="icono busqueda"></img>
                 </button>
             </div>
             <ul className="menu-horizontal">
-            <li>     
-            <a className="menu" href="#">Hola, nomb-usuario</a></li>
-            <ul className="menu-vertical">
-                <li> <a className="opciones" href="#">Inicio</a></li>
-                <li> <a className="opciones" href="#">Carrito de compra</a></li>
-                <li> <a className="opciones" href="#">Cerrar sesión</a></li>
+                <li>
+                    <a className="menu" href="#">Hola, nomb-usuario</a></li>
+                <ul className="menu-vertical">
+                    <li> <a className="opciones" href="#">Inicio</a></li>
+                    <li> <a className="opciones" href="#">Carrito de compra</a></li>
+                    <li> <a className="opciones" href="#">Cerrar sesión</a></li>
+                </ul>
             </ul>
-            </ul> 
 
-        </nav><div className="barra">
-        <img className="dispositivo" src="ico1.png" height= "50" width="130" alt="icono"></img>
-        <img className="dispositivo" src="ico2.png" height= "50" width="75" alt="icono"></img>
-        <img className="dispositivo" src="ico3.png" height= "55" width="90"alt="icono"></img>
-        <img className="dispositivo" src="ico4.png" height= "45" width="80" alt="icono"></img>
-        <img className="dispositivo" src="ico5.png" height= "55" width="100" alt="icono"></img>
-        <img className="dispositivo" src="ico6.png" height= "55" width="110" alt="icono"></img>
-        <img className="dispositivo" src="ico7.png" height= "65" width="120" alt="icono"></img>
+
+
+        </nav>
+        <div className="barra">
+                <img className="dispositivo" src="ico1.png" height="50" width="130" alt="icono"></img>
+                <img className="dispositivo" src="ico2.png" height="50" width="75" alt="icono"></img>
+                <img className="dispositivo" src="ico3.png" height="55" width="90" alt="icono"></img>
+                <img className="dispositivo" src="ico4.png" height="45" width="80" alt="icono"></img>
+                <img className="dispositivo" src="ico5.png" height="55" width="100" alt="icono"></img>
+                <img className="dispositivo" src="ico6.png" height="55" width="110" alt="icono"></img>
+                <img className="dispositivo" src="ico7.png" height="65" width="120" alt="icono"></img>
             </div>
             <body className="carrito">
+            <ShoppingCart />
 
             </body>
             <footer>
                 <div className="pie">
-            <img className="logo-footer" src="logo.png" alt="logo"></img>
-            <h2 className="titulo-footer">TECHNO WAREHOUSE</h2>
-            </div>
-            <p className="etiqueta-carrito">© 2024 Techno Warehouse. Todos los derechos reservados.</p>
-            </footer>
-            
-            </>
 
+                    <p className="etiqueta-carrito text-light ">© 2024 Techno Warehouse. Todos los derechos reservados.</p>
+
+                </div>
+            </footer>
+
+        </></>
     )
 }
 
